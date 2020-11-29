fc2dcb1e873b1fa1faef0d606aff5b24482bf492
VeraSkokova <miss.smiley17@gmail.com>
2020-11-29 21:09:56 +0700

    Task 4. Movies List

---
+++ app/src/main/java/ru/skokova/android_academy/FragmentMoviesDetails.kt
@@ -0,0 +1,29 @@
+package ru.skokova.android_academy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_movies_details, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.tv_back).apply {
+            setOnClickListener { requireActivity().onBackPressed() }
+        }
+        view.findViewById<ImageView>(R.id.img_back).apply {
+            setOnClickListener { requireActivity().onBackPressed() }
+        }
+    }
+}
