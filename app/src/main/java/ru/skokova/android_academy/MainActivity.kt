25e87e89e7ac31c09b21347bfd61d10bc9f9d797
VeraSkokova <miss.smiley17@gmail.com>
2020-12-16 21:45:57 +0700

    Movies List. Review

--- app/src/main/java/ru/skokova/android_academy/MainActivity.kt
+++ app/src/main/java/ru/skokova/android_academy/MainActivity.kt
@@ -3,7 +3,8 @@ package ru.skokova.android_academy
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity(), FragmentMoviesList.MovieClickListener {
+class MainActivity : AppCompatActivity(), FragmentMoviesList.MovieClickListener,
+    FragmentMoviesDetails.MovieDetailsNavigationListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
