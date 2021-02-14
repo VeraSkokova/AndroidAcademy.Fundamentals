193b07f3eba84008af401e25859d61d2c3b272e1
VeraSkokova <miss.smiley17@gmail.com>
2021-02-14 22:45:01 +0700

    Homework #9. Working from background. Merge fix

--- app/src/main/java/ru/skokova/android_academy/workers/MoviesWorkerFactory.kt
+++ app/src/main/java/ru/skokova/android_academy/workers/MoviesWorkerFactory.kt
@@ -6,6 +6,7 @@ import androidx.work.WorkerFactory
 import androidx.work.WorkerParameters
 import ru.skokova.android_academy.data.*
 import ru.skokova.android_academy.data.mapper.*
+import ru.skokova.android_academy.data.network.RetrofitModule
 
 class MoviesWorkerFactory : WorkerFactory() {
     override fun createWorker(
@@ -17,30 +18,37 @@ class MoviesWorkerFactory : WorkerFactory() {
         ImageLoadingInfoUpdateWorker::class.java.name -> ImageLoadingInfoUpdateWorker(
             appContext,
             workerParameters,
-            NetworkImageLoadingInfoRepository(JsonImageLoadingInfoMapper())
+            NetworkImageLoadingInfoRepository(
+                RetrofitModule.moviesApi,
+                JsonImageLoadingInfoMapper()
+            )
         )
         GenresUpdateWorker::class.java.name -> GenresUpdateWorker(
             appContext,
             workerParameters,
-            NetworkGenresRepository(JsonGenreMapper()),
+            NetworkGenresRepository(RetrofitModule.moviesApi, JsonGenreMapper()),
             DbGenresRepository(EntityGenreMapper())
         )
         MovieListUpdateWorker::class.java.name -> MovieListUpdateWorker(
             appContext,
             workerParameters,
-            NetworkMovieListRepository(JsonMovieMapper(), DbGenresRepository(EntityGenreMapper())),
+            NetworkMovieListRepository(
+                RetrofitModule.moviesApi,
+                JsonMovieMapper(),
+                DbGenresRepository(EntityGenreMapper())
+            ),
             DbMovieListRepository(EntityMovieMapper())
         )
         MovieDetailsUpdateWorker::class.java.name -> MovieDetailsUpdateWorker(
             appContext,
             workerParameters,
-            NetworkMovieDetailsRepository(JsonMovieMapper()),
+            NetworkMovieDetailsRepository(RetrofitModule.moviesApi, JsonMovieMapper()),
             DbMovieDetailsRepository(EntityMovieMapper())
         )
         ActorsUpdateWorker::class.java.name -> ActorsUpdateWorker(
             appContext,
             workerParameters,
-            NetworkActorsRepository(JsonActorsMapper()),
+            NetworkActorsRepository(RetrofitModule.moviesApi, JsonActorsMapper()),
             DbActorsRepository(EntityActorsMapper())
         )
         else -> throw IllegalStateException("Unknown worker with name $workerClassName")
